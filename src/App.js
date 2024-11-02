e5afa68ddce45fb9fdf01d867b7f0bb65d32e7b1
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-11-02 19:26:20 +0800

    Add compenent examples

--- src/App.js
+++ src/App.js
@@ -5,9 +5,23 @@ import React from "react";
 import MyList from "./components/MyList";
 
 function App() {
-  const jsx1 = '<h2 className="my-h2" title="hovering me">Hover me with your cursor</h2>';
+  const jsx1 =
+    '<h2 className="my-h2" title="hovering me">Hover me with your cursor</h2>';
   const jsx2 =
     ' gets translated to React.createElement("h2", {title: "hovering me", className: "my-h2"}, "Hover me with your cursor");';
+  const listItems = [
+    { id: "li1", text: "😀 List Item 1 Items above are hard coded" },
+    { id: "li2", text: "😃 List Item 2 But we are loaded dynamically" },
+    {
+      id: "li3",
+      text: '😄 List Item 3 listItems is passed as a prop named "items" to MyList',
+    },
+    {
+      id: "li4",
+      text: "😁 List Item 4 Use {} to merge JSX code (<MyList />) with some JS expression (listItems)",
+    },
+  ];
+
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
@@ -17,8 +31,13 @@ function App() {
       </h2>
       <p>{jsx1}</p>
       <p>{jsx2}</p>
-      <p>use className instead of class for CSS to differ from class in JavaScript </p>
-      <MyList />
+
+      <h1>Component</h1>
+      <p>
+        use className instead of class for CSS to differ from class in
+        JavaScript{" "}
+      </p>
+      <MyList items={listItems} />
     </div>
   );
 }
--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-const MyList = () => {
+const MyList = (props) => {
+  console.log(props.items);
+
   return (
     <ul className="my-list">
       <li>MyList is imported and used as a component with {"<MyList />"}</li>
@@ -8,7 +10,22 @@ const MyList = () => {
         Component name starts with capital letter to differ from normal HTML
         elements
       </li>
-      <li>styles in App.css are still applied but you create a MyList.css and add styles there</li>
+      <li>
+        styles in App.css are still applied but you create a MyList.css and add
+        styles there
+      </li>
+      <li>
+        props.items is passed to this component, press F12 to open DevTools if
+        you are using Chrome
+      </li>
+      <li>
+        go to the Console panel and you see {"▶ (4) [{…}, {…}, {…}, {…}]"}
+      </li>
+      <li>click the ▶ to expand</li>
+      <li>Below are elements rendered dynamically with {"{props.items.map((item) => {return <li key={item.id}>{item.text}</li>;})}"}</li>
+      {props.items.map((item) => {
+        return <li key={item.id}>{item.text}</li>;
+      })}
     </ul>
   );
 };
